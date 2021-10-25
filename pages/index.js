33fae9134ee964d7d91edbae234627f566a0f0a2
Prafullkumar Gaikwad <prafullg90@gmail.com>
2021-10-26 00:53:35 +0530

    Drop Images Component with dropper and validation

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,108 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React, { useCallback, useState } from 'react';
+import Head from 'next/head';
+
+import { useDropzone } from 'react-dropzone';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
+  const [droppedImages, setDroppedImages] = useState([]);
+  const [storeImgArr, setArr] = useState([]);
+  const [imgName, setImgName] = useState('');
+  const [imgDescr, setImgDescr] = useState('');
+  const [isError, setError] = useState('');
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  const onDrop = useCallback((acceptedFiles) => {
+    console.log('acceptedFiles', acceptedFiles);
+    const imgArr = [];
+    const imgStore = [];
+
+    acceptedFiles.forEach((file) => {
+      const reader = new FileReader();
+      imgArr.push(file.name);
+      reader.onload = () => {
+        const binaryStr = reader.result;
+        imgStore.push(binaryStr);
+      };
+    });
+    setArr(imgStore);
+    setDroppedImages(imgArr);
+  }, []);
+
+  const submitFiles = useCallback(() => {
+    setError('');
+    if (!imgName || !imgDescr) {
+      setError('Please add Image Name and description...!');
+      return;
+    }
+
+    if (droppedImages.length <= 0) {
+      setError('Please drop at least one image in dropbox...!');
+      return;
+    }
+
+    //localStorage.setItem('droppedImages', JSON.stringify(storeImgArr));
+  });
+
+  const renderDroppedImages = droppedImages.map((item) => <p>{item}</p>);
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Create Project</title>
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
+      <h1 className={styles.title}>Create Project</h1>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <div className={styles.card}>
+            <div {...getRootProps()}>
+              <input {...getInputProps()} />
+              <img
+                src='/folder_folder.png'
+                className={styles.folder_image}
+                alt='Folder Image'
+              />
+              <div className={styles.formText}>
+                {isDragActive ? (
+                  <p>Drop the files here ...</p>
+                ) : (
+                  <p>drag &amp; drop images here</p>
+                )}
+              </div>
+              {renderDroppedImages}
+            </div>
+          </div>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+          <div className={styles.form}>
+            <div className={styles.formText}>Name</div>
+            <input
+              type='text'
+              id='name'
+              name='name'
+              className={styles.inputText}
+              onChange={(e) => setImgName(e.target.value)}
+              value={imgName}
+              placeholder='Type Something...'
+            />
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            <div className={styles.formText}>Description</div>
+            <textarea
+              id='desc'
+              name='desc'
+              className={styles.inputTextArea}
+              placeholder='Type Something...'
+              onChange={(e) => setImgDescr(e.target.value)}
+              value={imgDescr}
+            />
+          </div>
         </div>
+        <div className={styles.isError}>{isError && isError}</div>
+        <button className={styles.button} onClick={() => submitFiles()}>
+          Create
+        </button>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
